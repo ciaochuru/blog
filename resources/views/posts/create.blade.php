c00dab132e57b78643e27b3e3fdc7b2f3a3526a6
ちゅーる <matsunaga0316.r@gmail.com>
2024-05-23 01:06:55 +0900

    ブログ投稿の作成

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,11 +12,13 @@
             @csrf
             <div class="title">
                 <h2>タイトル：<h2>
-                <input type="text" name="post[title]" placeholder="タイトルを入力">
+                <input type="text" name="post[title]" placeholder="タイトルを入力" value="{{ old('post.title') }}">
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>メッセージ：<h2>
-                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" ></textarea><br>
+                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。">{{ old('post.body') }}</textarea><br>
+                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="store">
         </form>

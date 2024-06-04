5943ca9337d17404dea12edb47fe76b1dcc7b6de
ちゅーる <matsunaga0316.r@gmail.com>
2024-06-05 03:23:14 +0900

    ログイン機能2

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,40 +6,44 @@
         <!--Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>カテゴリー別</h1>
-        <div class="header">
-            <a href="/posts/create">[create]</a>
-        </div>
-        <div class="posts">
-            @foreach ($posts as $post)
-                <h2 class="title">
-                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
-                <p class="body">{{ $post->body }}</p>
-                <div class="category">
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                </div>
-                <div class="delete">
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <input type="button" onclick="deletePost({{ $post->id }})" value="delete" />
-                    </form>
-                </div>
-            @endforeach
-            <div class="footer">
-                <a href="/">投稿一覧に戻る</a>
+    <x-app-layout>
+        <body>
+            <div class="header">
+                <h1>カテゴリー別</h1>
             </div>
-        </div>
-        <div class="paginate">{{ $posts->links() }}</div>
-        <script>
-            function deletePost(id){
-                'use strict'
-                if(confirm('削除すると復元できません。 \n本当に削除しますか？')){
-                    document.getElementById(`form_${id}`).submit();
+            <div class="create">
+                <a href="/posts/create">[create]</a>
+            </div>
+            <div class="posts">
+                @foreach ($posts as $post)
+                    <h2 class="title">
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class="body">{{ $post->body }}</p>
+                    <div class="category">
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    </div>
+                    <div class="delete">
+                        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <input type="button" onclick="deletePost({{ $post->id }})" value="delete" />
+                        </form>
+                    </div>
+                @endforeach
+                <div class="footer">
+                    <a href="/">投稿一覧に戻る</a>
+                </div>
+            </div>
+            <div class="paginate">{{ $posts->links() }}</div>
+            <script>
+                function deletePost(id){
+                    'use strict'
+                    if(confirm('削除すると復元できません。 \n本当に削除しますか？')){
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
+    </x-app-layout>
 </html>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,35 +6,39 @@
         <!--Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>投稿</h1>
-        <div class="content">
-            <form action="/posts" method="POST">
-                @csrf
-                <div class="content_title">
-                    <h2>タイトル：<h2>
-                    <input type="text" name="post[title]" placeholder="タイトルを入力" value="{{ old('post.title') }}">
-                    <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
-                </div>
-                <div class="content_body">
-                    <h2>メッセージ：<h2>
-                    <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。">{{ old('post.body') }}</textarea><br>
-                    <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
-                </div>
-                <div class="categories">
-                    <h3>カテゴリー：</h3>
-                    <select name="post[category_id]">
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <input type="submit" value="store">
-            </form>
-        </div>
-        
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+    <x-app-layout>
+        <body>
+            <div class="create">
+                <h1>投稿</h1>
+            </div>
+            <div class="content">
+                <form action="/posts" method="POST">
+                    @csrf
+                    <div class="content_title">
+                        <h2>タイトル：<h2>
+                        <input type="text" name="post[title]" placeholder="タイトルを入力" value="{{ old('post.title') }}">
+                        <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                    </div>
+                    <div class="content_body">
+                        <h2>メッセージ：<h2>
+                        <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。">{{ old('post.body') }}</textarea><br>
+                        <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                    </div>
+                    <div class="categories">
+                        <h3>カテゴリー：</h3>
+                        <select name="post[category_id]">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <input type="submit" value="store">
+                </form>
+            </div>
+            
+            <div class="footer">
+                <a href="/">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
